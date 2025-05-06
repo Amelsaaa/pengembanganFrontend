59f9669164f428d51b5be7df0975667afaafb2ef
Amelsaaa <amandamelda76@gmail.com>
2025-05-06 11:13:00 +0700

    Add files via upload

---
+++ react-grup-new/src/payment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bayar = (prop) => {
+  return (
+    <div>
+      <img src={prop.gambar} alt="Metode Pembayaran" width="65" />
+    </div>
+  );
+};
+
+export default Bayar;
